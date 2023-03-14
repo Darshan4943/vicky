bcc625df6c65e4f28129b8baa55c1117d9d0fd60
Darshan4943 <darshanshinde4943@gmail.com>
2023-03-14 23:09:36 +0530

    React Day 6 Assignment_Todo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,70 @@
-import React, { Fragment,useState} from 'react'
+import React,{useState} from 'react';
+
 
 import './App.css'
-export default function App(){
-
-  const [input,setInput]= useState([])
-  let result
 
 
-  function handleChange(event){
-    return(result=event.target.value)
-  }
 
-  function handleSubmit(){
-    return setInput([...input,result])
-  }
-
-  const margin={margin:20}
-  
-  return(
-    <Fragment>
-      <input onChange={handleChange}/>
-      
-      <button style={margin} onClick={handleSubmit}>Add</button>
-      
-      {input.map((number)=>(<p>{number}</p>))}
-      <br/>
-      
-    </Fragment>
-   
-
-    
+function App(){
+  const [input, setInput] = useState([])
+  const [count, setCount] = useState(0)
+ 
+  let result 
+  function handleTodo(event){
+    return(
+      result=event.target.value
     )
+  }
+
+  
+  function submitTodo(){
+    setCount(count+1)
+      return setInput([...input,result])
+  
+  }
+  
+  
+  function handleDelete(abc){
+    const result=input.filter((num)=>num!==abc)
+    setCount(count-1)
+      return  setInput(result)
+  
+  }
+  const handleComplete = (id) => { 
+    setInput(input => input.map(num => {
+      if (num === id) {
+        return (
+          <span style={{textDecoration:"line-through"}}>
+            {num}  
+          </span>
+        )
+      }
+       else {
+        return num;
+      }
+    }));
+    setCount(count-1)
+  };
+
+  return(
+    <div className='container'>
+        <div className='box'>
+          <h1 >Pending Tasks - {(count)}</h1>
+            <div className='box-inside'>
+              {input.map((num)=>(
+                <p key={num}>{num}
+                  <span>
+                    <button className='btn-cmplte' onClick={() => handleComplete(num)}>Complete</button>
+                    <button className='btn-delete' onClick={() => handleDelete(num)}>Dlt</button>
+                  </span>
+                </p>))}
+            </div>
+          <input onChange={handleTodo}/>
+          <button onClick={submitTodo}>Submit</button>
+        </div>
+    </div>
+  )
 }
+
+
+export default App;
