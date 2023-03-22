a90111be12ccca0a7e648ae9496520ba719ca760
Darshan4943 <darshanshinde4943@gmail.com>
2023-03-22 23:52:16 +0530

    React CSS Tic Tac Toe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
-import Header from './headerComponent/header.js'
-import Text from './textComponent/text.js'
+import Tictactoe from './Component/ticTacToe.js'
+import Scroller from './textComponent/Scroller.js'
+import './App.css'
 function App() {
 
   
   return (
     <>
-      <Header/>
-      <Text/>
+      <h1>problem 1</h1>
+      <Tictactoe/>
+      <h1>Problem 2</h1>
+      <Scroller/>
+      <h2 className='head'>Scroll with left, right arrow key</h2>
+      
     </>
     )
 }
