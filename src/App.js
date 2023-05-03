73d6ce181074536e7794035b72d5eae53f1000f6
Darshan <darshanshinde4943@gmaul.com>
2023-05-03 22:54:33 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
-import * as React from "react";
-import Select from "react-select"
-
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
-
-    const [value,setValue]=React.useState(null)
-  const options=[
-    {value:"CHOCOLATE",label:"Chocolate"},
-    {value:"CANDY",label:"Candy"},
-    {value:"ICE-CREAME",label:"Ice Cream"},
-    {value:"BURGER",label:"Burger"}
-    
-  ]
+  let numbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    ".",
+  ];
+  // let operators=[,]
+  const [value, setValue] = useState("");
+  const [count, setcount] = useState(0);
+  function handleClick(id) {
+    if (value === "0") {
+      setValue(numbers[id]);
+    } else {
+      if (count === 1) {
+        setcount(0);
+        setValue(numbers[id]);
+        return;
+      }
+      setValue(value + numbers[id]);
+    }
+  }
+  function handleres() {
+    const result = eval(value);
+    setValue(result);
+    setcount(count + 1);
+  }
+  function handleClear() {
+    setValue("");
+  }
   return (
-    <div style={{margin:20,width:200}}>
-       <Select 
-       options={options} 
-       defaultValue={value} 
-       placeholder="Select your Snack" 
-       onChange={setValue} 
-       isMulti
-       isSearchable
-       noOptionsMessage={()=>"No Snacks Found"}
-      
-       styles={{
-        clearIndicator:()=>({
-          color:"red"
-        })
-       }}
-       />
+    <div className="container">
+      <input value={value} />
+      <div className="numbers">
+        {numbers.map((num, index) => (
+          <button key={index} onClick={() => handleClick(index)}>
+            {num}
+          </button>
+        ))}
+        <button onClick={handleres}>=</button>
+        <button onClick={handleClear}>clear</button>
+      </div>
     </div>
   );
 }
