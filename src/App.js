5c451c23e7e85079fdedb46767cd054c7fa44b03
Darshan <darshanshinde4943@gmaul.com>
2023-05-05 08:23:28 +0530

    calender

--- src/App.js
+++ src/App.js
@@ -1,60 +1,16 @@
-import { useState } from "react";
-import "./App.css";
-
+import React,{ useState } from "react";
+import Calendar from "react-calendar";
+import "./App.css"
 export default function App() {
-  let numbers = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "+",
-    "-",
-    "*",
-    "/",
-    "%",
-    ".",
-  ];
-  // let operators=[,]
-  const [value, setValue] = useState("");
-  const [count, setcount] = useState(0);
-  function handleClick(id) {
-    if (value === "0") {
-      setValue(numbers[id]);
-    } else {
-      if (count === 1) {
-        setcount(0);
-        setValue(numbers[id]);
-        return;
-      }
-      setValue(value + numbers[id]);
-    }
-  }
-  function handleres() {
-    const result = eval(value);
-    setValue(result);
-    setcount(count + 1);
-  }
-  function handleClear() {
-    setValue("");
-  }
+  const [date, setDate] = useState(new Date());
+
   return (
-    <div className="container">
-      <input value={value} />
-      <div className="numbers">
-        {numbers.map((num, index) => (
-          <button key={index} onClick={() => handleClick(index)}>
-            {num}
-          </button>
-        ))}
-        <button onClick={handleres}>=</button>
-        <button onClick={handleClear}>clear</button>
+    <div className="app">
+      <h1 className="header">Calendar </h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} />
       </div>
+      <div className="text-center">Today's date: {date.toDateString()}</div>
     </div>
   );
 }
